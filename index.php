d4a691e0587ef8530f3a201401925c987c7b1624
Tom McFarlin <tom@tommcfarlin.com>
2023-05-31 16:39:09 -0400

    Format for coding standards

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
+
 /**
  * Index file. Start it all.
  */
+
 namespace ShareSecret;
 
-require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 $share_secret = new ShareSecretFactory();
 $share_secret->create_share_secret()->init();
